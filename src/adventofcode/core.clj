b7583e9e57360abd160ecd85f833bebf45bbcc64
ontanj <anton.jeppsson@outlook.com>
2020-12-11 19:56:26 +0100

    add solution task 1.1

--- src/adventofcode/core.clj
+++ src/adventofcode/core.clj
@@ -1,7 +1,31 @@
 (ns adventofcode.core
-  (:gen-class))
+  (:require [clojure.string :as str]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def line-split
+  #(str/split %1 #"\n"))
+
+(defn in? 
+  "true if coll contains elm"
+  [coll elm]
+  (some #(= elm %) coll))
+
+; task 1
+(defn find-sum
+  [coll sum]
+  (let [val (first coll)
+        neg (- sum val)]
+    (if (in? (rest coll) neg)
+      [val neg]
+      (recur (rest coll) sum))))
+
+(defn verify-find-sum
+  [values coll sum]
+  (every? identity (map (partial in? coll) values)))
+
+(defn t1-1
+  []
+  (let [sum 2020
+        data (map read-string (line-split (slurp "inputs/input1")))
+        results (find-sum data sum)]
+    (when (verify-find-sum results data sum)
+      (apply * results))))
